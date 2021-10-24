8992c5fd8969a886728e9c457adec45ac3a567c3
Dhananjay Tanpure <dtan13@pm.me>
2021-10-24 09:18:46 +0530

    modified scripts to publish and build

--- build.sh
+++ build.sh
@@ -2,19 +2,9 @@
 
 DIR=$(pwd)
 
-if [[ $(git status -s) ]]; then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1
-fi
-
 echo "Deleting old publication"
 rm -rf public
 mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -31,6 +21,3 @@ hugo
 
 echo "Restore changes in theme"
 cd $DIR/themes/uBlogger && git restore . && cd $DIR
-
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to blog.dtan13.tech" && cd ..
